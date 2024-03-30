ba2b7acf6d82772a58e0dcaadef82a0ee52ecb36
Thanawan Wongchanasri <thanawan@orcsoft.co.th>
2024-03-30 22:10:40 +0700

    submit assignment JS-boolean-and-logical-operations

--- ex-1.js
+++ ex-1.js
@@ -3,6 +3,8 @@ let hasCriminalBlacklist;
 
 let isAllow;
 
-// Start coding here
+isOver18 = true;
+hasCriminalBlacklist = true;
+isAllow = isOver18 && hasCriminalBlacklist;
 
 console.log(isAllow);
--- ex-2.js
+++ ex-2.js
@@ -7,6 +7,16 @@ let hasBoughtProductFromITCategory;
 let hasAttendedDiscountEvent;
 let isPlatinum;
 
-let hasPromotion;
+lastMonthPaidMoreThan4000 = true;
+isWeekday = true;
+hasBoughtProductFromITCategory = false;
+hasAttendedDiscountEvent = true;
+
+isPlatinum = false;
+
+let firstCase = lastMonthPaidMoreThan4000 && isWeekday && !hasBoughtProductFromITCategory && !hasAttendedDiscountEvent;
+let secondCase = isPlatinum;
+
+let hasPromotion = firstCase || secondCase;
 
 console.log(hasPromotion);
